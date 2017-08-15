36c340975a4b7bf61517408afb23d02f1e8bee22
Linh Phung <teivalp@gmail.com>
2017-08-14 20:41:52 -0700

    Adding WebElement and WebDriver extensions.

---
+++ ProtractorDemo/WebDriverExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace FoxSportsSite
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebElement FindElement(this IWebDriver Driver, By By, int TimeoutInSeconds)
+        {
+            if (TimeoutInSeconds > 0)
+            {
+                WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(TimeoutInSeconds));                
+                return wait.Until(ExpectedConditions.ElementIsVisible(By));
+            }
+            return Driver.FindElement(By);
+        }
+        /// <summary>
+        /// Find and wait for element to appear.
+        /// Catch NoSuchElementException if thrown
+        /// </summary>
+        /// <param name="Driver">Browser interface set by test</param>
+        /// <param name="By">Object set by test to locate element</param>
+        /// <returns>True/False</returns>
+        public static IWebElement IsElementPresent(this IWebDriver Driver, By By)
+        {
+            try
+            {
+                return FindElement(Driver, By, 5);
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ProtractorDemo/WebElementExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace FoxSportsSite
+{
+    public static class WebElementExtensions
+    {
+        /// <summary>
+        /// Create a right click action for the designated IWebElement
+        /// </summary>
+        /// <param name="Element">IWebElement</param>
+        /// <param name="Driver">Interface controlling browser</param>
+        public static void ContextMenu(this IWebElement Element, IWebDriver Driver)
+        {
+            // Create Actions object            
+            Actions builder = new Actions(Driver);
+            // Use ContextClick method to open context menu of IWebElement
+            builder.ContextClick(Element).Build().Perform();
+        }
+        /// <summary>
+        /// Hover mouse icon without performing a click
+        /// </summary>
+        /// <param name="Element"></param>
+        /// <param name="Driver">Interface controlling browser</param>
+        public static void Hover(this IWebElement Element, IWebDriver Driver)
+        {            
+            Actions action = new Actions(Driver);
+            action.MoveToElement(Element).Perform();
+        }
+    }
+}
